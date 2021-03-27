5112663c8529f4457fd175a58224b2a55e26e60e
DJ Hersh <DanielJHersh@gmail.com>
2021-03-27 15:46:00 -0700

    Added ability to shorten summaries based on width of viewport

--- src/components/GameInfo/GameInfo.tsx
+++ src/components/GameInfo/GameInfo.tsx
@@ -9,21 +9,39 @@ const GameInfo: React.FC<GameData> = ({
   rating,
   summary,
 }) => {
+  const [width, setWidth] = useState(window.innerWidth);
   const [formattedDate, setFormattedDate] = useState("");
   const [summaryPreview, setSummaryPreview] = useState(summary);
 
   useEffect(() => {
-    if (summary.split(" ").length > 55) {
-      let shortened = summary.split(" ");
-      shortened.splice(55);
-      setSummaryPreview(shortened.join(" ") + "...");
-    };
+    window.addEventListener("resize", () => {
+      setWidth(window.innerWidth);
+    });
+  }, [window.innerWidth]);
+
+  useEffect(() => {
     const date = new Date(first_release_date);
     const day = date.getDate();
     const month = date.getMonth() + 1;
     const year = date.getFullYear();
     setFormattedDate(`${day}/${month}/${year}`);
-  });
+  }, []);
+
+  useEffect(() => {
+    if (width < 755) {
+      if (summary.split(" ").length > 30) {
+        let shortened = summary.split(" ");
+        shortened.splice(20);
+        setSummaryPreview(shortened.join(" ") + "...");
+      };
+    } else {
+      if (summary.split(" ").length > 55) {
+        let shortened = summary.split(" ");
+        shortened.splice(55);
+        setSummaryPreview(shortened.join(" ") + "...");
+      };
+    }
+  }, [width]);
 
   return (
     <div className="row align-items-center mb-2 ml-lg-5 panel">
@@ -33,7 +51,7 @@ const GameInfo: React.FC<GameData> = ({
         <p className="mt-n1">Release Date: {formattedDate}</p>
         <p>[Summary] {summaryPreview}</p>
       </div>
-      <div className="col-1 mt-n4 pl-4 pl-lg-0 pl-xl-5">
+      <div className="col-1 mt-n4 pl-md-4 pl-lg-0 pl-xl-5">
         <p className="text-center pt-1 rating">{rating.toString()[0]}</p>
       </div>
     </div>
