439f16dd43149208adb082cb6658ef4b1eeaaae9
DJ Hersh <DanielJHersh@gmail.com>
2021-03-27 16:15:21 -0700

    Made the gameInfo component responsive

--- src/components/GameInfo/GameInfo.tsx
+++ src/components/GameInfo/GameInfo.tsx
@@ -33,26 +33,38 @@ const GameInfo: React.FC<GameData> = ({
         let shortened = summary.split(" ");
         shortened.splice(20);
         setSummaryPreview(shortened.join(" ") + "...");
-      };
+      }
     } else {
       if (summary.split(" ").length > 55) {
         let shortened = summary.split(" ");
         shortened.splice(55);
         setSummaryPreview(shortened.join(" ") + "...");
-      };
+      }
     }
   }, [width]);
 
   return (
-    <div className="row align-items-center mb-2 ml-lg-5 panel">
-      <div className="col-2 black"></div>
-      <div className="col-9 pb-4">
-        <h5 className="white">{name}</h5>
-        <p className="mt-n1">Release Date: {formattedDate}</p>
-        <p>[Summary] {summaryPreview}</p>
+    <div>
+      <div className="row align-items-center mb-2 ml-lg-5 panel">
+        <div className="col-2 black"></div>
+        <div className="col-9 pb-4">
+          <h5 className="white">{name}</h5>
+          <p className="mt-n1">Release Date: {formattedDate}</p>
+          <p>[Summary] {summaryPreview}</p>
+        </div>
+        <div className="col-1 mt-n4 pl-md-4 pl-lg-0 pl-xl-5">
+          <p className="text-center pt-1 rating">{rating.toString()[0]}</p>
+        </div>
       </div>
-      <div className="col-1 mt-n4 pl-md-4 pl-lg-0 pl-xl-5">
-        <p className="text-center pt-1 rating">{rating.toString()[0]}</p>
+      <div className="smallPanel">
+        <div className="smallBlack">
+          <p className="text-center pt-1 smallRating">{rating.toString()[0]}</p>
+        </div>
+        <div className="px-3 pt-2">
+        <h5 className="white">{name}</h5>
+          <p className="mt-n1">Release Date: {formattedDate}</p>
+          <p>[Summary] {summaryPreview}</p>
+          </div>
       </div>
     </div>
   );
